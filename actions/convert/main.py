f5cca66bf845f025ffd17942ccb8420b1ab1e32f
Mostafa Moradian <mostafa@grafana.com>
2025-01-10 11:46:17 +0100

    Remove output_dir input param in favor of the folders.conversion_path in config file

--- actions/convert/main.py
+++ actions/convert/main.py
@@ -6,10 +6,15 @@ from settings import load_config, parse_args
 if __name__ == "__main__":
     # Parse command line arguments
     args = parse_args()
+    config = load_config(args.config)
+
+    # Get the conversions output directory from the config file.
+    conversions_output_dir = config.get("folders.conversion_path", "conversions")
+
     # Convert Sigma rules to the target format per each conversion object in the config
     convert_rules(
-        config=load_config(args.config),
+        config=config,
         path_prefix=args.path_prefix,
-        conversions_output_dir=args.conversions_output_dir,
+        conversions_output_dir=conversions_output_dir,
         render_traceback=args.render_traceback,
     )
