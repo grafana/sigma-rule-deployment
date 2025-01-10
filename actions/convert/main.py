d62de32cfa9df32375fd0b538b2c6c2e6a9c2637
Mostafa Moradian <mostafa@grafana.com>
2025-01-10 12:58:07 +0100

    Join paths using pathlib.Path

--- actions/convert/main.py
+++ actions/convert/main.py
@@ -1,12 +1,14 @@
 """Main entrypoint for the convert script."""
 
+from pathlib import Path
+
 from convert import convert_rules
 from settings import load_config, parse_args
 
 if __name__ == "__main__":
     # Parse command line arguments
     args = parse_args()
-    config = load_config(args.path_prefix / args.config)
+    config = load_config(str(Path(args.path_prefix) / Path(args.config)))
 
     # Get the conversions output directory from the config file.
     conversions_output_dir = config.get("folders.conversion_path", "conversions")
