4d1106b786faea40378296bd552315345a66e4ec
James Crocker <87319125+jamesc-grafana@users.noreply.github.com>
2025-03-18 10:59:23 +0000

    update action to pull docker composite

--- actions/entrypoint.sh
+++ actions/entrypoint.sh
@@ -22,6 +22,30 @@ case "$1" in
         echo "Deploying Sigma rules"
         deploy "$@"
         ;;
+    "convert")
+        shift
+        echo "Converting Sigma rules"
+        plugin_packages=${PLUGIN_PACKAGES}
+        valid_plugins=()
+
+        shopt -s nocasematch
+        for plugin in $(echo $plugin_packages | tr ',' ' '); do
+            if [[ "$plugin" == pysigma-backend-* ]]; then
+                valid_plugins+=("$plugin")
+            else
+                echo "Error: Invalid plugin name: $plugin"
+                exit 1
+            fi
+        done
+        shopt -u nocasematch
+
+        if [ ${#valid_plugins[@]} -gt 0 ]; then
+            uv add --directory /app/convert "${valid_plugins[@]}"
+        fi
+
+        uv run --directory /app/convert main.py --config ${CONFIG_PATH} --render-traceback ${RENDER_TRACEBACK}
+
+        ;;
     *)
         echo "Invalid argument: $1"
         exit 1
